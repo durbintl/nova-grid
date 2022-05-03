115d2b7b6fd413d1fde4cfc301e854cc0e4302ca
Binh Nguyen <nguyen.binh@ebuynow.com>
2022-05-03 16:02:06 +0700

    fix

--- resources/js/tool.js
+++ resources/js/tool.js
@@ -1,5 +1,3 @@
-import DefaultField from './components/DefaultField'
-
 Nova.booting((Vue, router, store) => {
-    Vue.component('default-field', DefaultField)
-})
+  Vue.component("default-field", require("./components/DefaultField").default);
+});
